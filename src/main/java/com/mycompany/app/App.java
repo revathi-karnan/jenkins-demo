eb3e8eccfedae305e578f886c84327af1e8d1fc9
revathi-karnan <revathi.karnan@gmail.com>
2023-07-12 19:14:48 +0000

    "App.java changed"

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World from Maven Git Java and Jenkins!" );
+        System.out.println( "Hello World from Maven Git Java and Jenkins! and i enjoy learning them" );
     }
 }
